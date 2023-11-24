683911c9cc48cb2d2d1eb708176aba30876570a7
michel <michel.launier@icloud.com>
2023-11-24 02:46:16 -0500

    Initial commit of the-fabric

---
+++ the-fabric/the-fabric/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  the-fabric
+//
+//  Created by Michel Launier on 2023-11-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ the-fabric/the-fabric/the_fabricApp.swift
@@ -0,0 +1,17 @@
+//
+//  the_fabricApp.swift
+//  the-fabric
+//
+//  Created by Michel Launier on 2023-11-24.
+//
+
+import SwiftUI
+
+@main
+struct the_fabricApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
