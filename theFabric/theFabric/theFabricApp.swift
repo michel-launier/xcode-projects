527cb5c4c30fccf4379afe026776a37d583bcc55
michel <michel.launier@icloud.com>
2023-11-24 02:49:45 -0500

    theFabric initial commit

---
+++ theFabric/theFabric/theFabricApp.swift
@@ -0,0 +1,17 @@
+//
+//  theFabricApp.swift
+//  theFabric
+//
+//  Created by Michel Launier on 2023-11-24.
+//
+
+import SwiftUI
+
+@main
+struct theFabricApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
