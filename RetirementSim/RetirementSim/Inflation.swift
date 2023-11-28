feaf0fe37ecec9a51f2b2b134c0e65d521bc2abc
michel <michel.launier@icloud.com>
2023-11-28 04:34:10 -0500

    Add draft Inflation type

---
+++ RetirementSim/RetirementSim/Inflation.swift
@@ -0,0 +1,15 @@
+//
+//  Inflation.swift
+//  RetirementSim
+//
+//  Created by Michel Launier on 2023-11-28.
+//
+
+import Foundation
+
+struct Inflation {
+    var history: [(year: Int, inflation: Float)]
+    var average: Float {
+        return 0.0
+    }
+}
