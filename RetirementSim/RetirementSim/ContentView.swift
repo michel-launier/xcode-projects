c9b53bf5d3eef812feeca48581684fce88df4fb9
michel <michel.launier@icloud.com>
2023-11-28 04:25:55 -0500

    Added project RetirementSim focused on simulating the savings and retirement income.

---
+++ RetirementSim/RetirementSim/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RetirementSim
+//
+//  Created by Michel Launier on 2023-11-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RetirementSim/RetirementSim/RetirementSimApp.swift
@@ -0,0 +1,17 @@
+//
+//  RetirementSimApp.swift
+//  RetirementSim
+//
+//  Created by Michel Launier on 2023-11-28.
+//
+
+import SwiftUI
+
+@main
+struct RetirementSimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
